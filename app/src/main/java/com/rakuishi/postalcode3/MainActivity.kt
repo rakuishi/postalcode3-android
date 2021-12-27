5c76c86420b18480118878b4295e5d0e20e81119
rakuishi <rakuishi@gmail.com>
2021-12-27 10:06:12 +0900

    Create new project with Fragment + ViewModel

---
+++ app/src/main/java/com/rakuishi/postalcode3/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rakuishi.postalcode3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rakuishi.postalcode3.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/rakuishi/postalcode3/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.rakuishi.postalcode3.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
