6069415ad0b5f78a094bea29640d1ae8b4c67919
rakuishi <rakuishi@gmail.com>
2021-12-29 08:47:08 +0900

    Update theme palette

--- app/src/main/java/com/rakuishi/postalcode3/presentation/theme/Color.kt
+++ app/src/main/java/com/rakuishi/postalcode3/presentation/theme/Color.kt
@@ -2,7 +2,5 @@ package com.rakuishi.postalcode3.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Red = Color(0xFFEA4335)
+val RedVariant = Color(0xFFD23C2F)
