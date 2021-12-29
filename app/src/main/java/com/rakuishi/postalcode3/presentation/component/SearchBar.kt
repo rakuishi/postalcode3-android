df91252372c9b877468835484b5851e163999444
rakuishi <rakuishi@gmail.com>
2021-12-29 16:40:30 +0900

    Rename SearchView -> SearchBar

---
+++ app/src/main/java/com/rakuishi/postalcode3/presentation/component/SearchBar.kt
@@ -0,0 +1,101 @@
+package com.rakuishi.postalcode3.presentation.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rakuishi.postalcode3.R
+import com.rakuishi.postalcode3.presentation.theme.AppTheme
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun SearchBarPreview() {
+    AppTheme {
+        Surface {
+            SearchBar()
+        }
+    }
+}
+
+@Composable
+fun SearchBar(
+    query: String = "",
+    onQueryChanged: (String) -> Unit = {},
+    onKeyboardDone: () -> Unit = {}
+) {
+    val focusManager = LocalFocusManager.current
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        shadowElevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colorScheme.primaryContainer
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = query,
+            placeholder = {
+                Text(
+                    modifier = Modifier.alpha(ContentAlpha.medium),
+                    text = stringResource(id = R.string.empty_search),
+                )
+            },
+            textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurface),
+            singleLine = true,
+            leadingIcon = {
+                Icon(Icons.Filled.Search, contentDescription = "")
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                focusedLabelColor = Color.Transparent,
+                cursorColor = MaterialTheme.colorScheme.primary,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+            ),
+            onValueChange = {
+                onQueryChanged.invoke(it)
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    onKeyboardDone.invoke()
+                    focusManager.clearFocus()
+                }
+            )
+        )
+    }
+}
--- app/src/main/java/com/rakuishi/postalcode3/presentation/ui/search/SearchScreen.kt
+++ app/src/main/java/com/rakuishi/postalcode3/presentation/ui/search/SearchScreen.kt
@@ -1,18 +1,15 @@
 package com.rakuishi.postalcode3.presentation.ui.search
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.rakuishi.postalcode3.database.PostalCode
 import com.rakuishi.postalcode3.presentation.component.PostalCodeListItem
-import com.rakuishi.postalcode3.presentation.component.SearchView
+import com.rakuishi.postalcode3.presentation.component.SearchBar
 import com.rakuishi.postalcode3.presentation.theme.AppTheme
 import timber.log.Timber
 
@@ -21,15 +18,11 @@ fun SearchScreen(viewModel: SearchViewModel) {
     AppTheme {
         Surface {
             Column(modifier = Modifier.fillMaxSize()) {
-                Box(
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
-                ) {
-                    SearchView(
-                        viewModel.query.value,
-                        onQueryChanged = { viewModel.onQueryChanged(it) },
-                        onKeyboardDone = { viewModel.search() }
-                    )
-                }
+                SearchBar(
+                    viewModel.query.value,
+                    onQueryChanged = { viewModel.onQueryChanged(it) },
+                    onKeyboardDone = { viewModel.search() }
+                )
                 PostalCodeList(viewModel.postalCodeList.value)
             }
         }
